ef31a317305eb6b90321b264f714d0cdea972aea
PietroAlex4 <pietroalexrod4@gmail.com>
2026-02-26 18:24:57 -0300

    att

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,8 +13,12 @@ const geistMono = Geist_Mono({
 
 export default function Home() {
   return (
-   <div className="min-h-screen min-w-full bg-slate-500">
-    <h2 className="text-4xl text-green-400">Title</h2>
+   <div className={`${geistSans.variable} ${geistMono.variable} 
+   grid grid-rows-[20px_1fr_20px] 
+   items-center justify-items-center 
+   min-h-screen p-8 pb-20 gap-16 sm:p-20 
+   font-[family-name:var(--font-geist-sans)]`}>
+    <h2>Hello World</h2>
    </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,5 +10,5 @@ export default {
     extend: {},
   },
   plugins: [],
-}
+} satisfies Config
 
